28b0148dc3374698feb3043002c8543186b4992e
Lightbells <takahasi0928@gmail.com>
2022-11-23 17:30:21 +0900

    main.pyをコミットした

--- SecondHalf/experiments/src/main.py
+++ SecondHalf/experiments/src/main.py
@@ -5,22 +5,21 @@ def main() -> None:
     print("Starting Trials")
     print("=========================================")
 
-    iteratations = 10
     configurations = {
        "differential_evolution": {
            "maxiter": 1000,
-           "canditate_populations": [10, 20, 50, 100, 200, 500, 800, 1000],
+           "canditate_populations": [10, 20, 50, 100, 200, 500],
            "canditate_recombination_rates": [0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95],
-           "canditate_mutation_rates": [0.001, 0.02, 0.04, 0.06, 0.08, 0.10, 0.18, 0.20],
+           "canditate_mutation_rates": [0.001, 0.02, 0.04, 0.08, 0.10, 0.20],
        },
        "genetic_algorithm": {
            "generations": 100,
-           "canditate_populations": [10, 20, 50, 100, 200, 500, 800, 1000],
+           "canditate_populations": [10, 20, 50, 100, 200, 500],
            "canditate_crossover_rates": [0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95],
-           "canditate_mutation_rates": [0.001, 0.02, 0.04, 0.06, 0.08, 0.10, 0.18, 0.20],
+           "canditate_mutation_rates": [0.001, 0.02, 0.04, 0.08, 0.10, 0.20],
        },
        "particle_swarm_optimization": {
-               "canditate_populations": [10, 20, 50, 100, 200, 500, 800, 1000],
+               "canditate_populations": [10, 20, 50, 100, 200, 500],
                "canditate_c1": [0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9],
                "canditate_c2": [0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9],
                "canditate_w": [0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9],
@@ -29,7 +28,7 @@ def main() -> None:
        "seed": 1234,
     }
 
-    logs = trials(iteratations, configurations)
+    logs = trials(configurations)
 
     print("=========================================")
     print("Writing Logs...")
