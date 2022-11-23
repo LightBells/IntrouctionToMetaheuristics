2a5f646bc49b0f0b7034ef515dc0f2f98b27329f
Lightbells <takahasi0928@gmail.com>
2022-11-23 22:41:20 +0900

    Quarticを修正した

--- SecondHalf/experiments/src/functions/quartic.py
+++ SecondHalf/experiments/src/functions/quartic.py
@@ -3,5 +3,5 @@ def quartic(x):
     value = 0.0
     for i in range(len(x)):
         value += (i+1)*x[i]**4
-        value += random.gauss(0, 1)
+    value += random.gauss(0, 1)
     return [value]
